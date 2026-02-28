99bd65f46d9cdc38f84a696f9da790fbe692cd1d
pinkot <pinkot021@gmail.com>
2026-02-28 15:05:14 +0300

    работа с шаблонизатором

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
         path('', views.catalog_info),
         path('<int:pk>', views.catalog_detail),
+        path('test/', views.catalog_detail_test),
         path('<slug:category>', views.catalog_category),
-
-
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -15,6 +15,29 @@ def main_page(request):
 def catalog_detail(request, pk):
     return HttpResponse(f"Страница номер {pk} каталога")
 
+
+def catalog_detail_test(request):
+
+    template_name = 'catalog/catalog_detail.html'
+
+    title = "test_title"
+    promo_products = [
+        'promo_product',
+        'promo_product2',
+        'promo_product3',
+        'promo_product4',
+    ]
+
+    context = {
+        'title': title,
+        'promo_products': promo_products,
+    }
+
+    return render(
+        request=request,
+        template_name=template_name,
+        context=context,
+    )
+
 def catalog_category(request, category):
     return HttpResponse(f"Страница категории {category}")
-
